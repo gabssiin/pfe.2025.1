f09cd399325ca5b044e406bcd193cebaf615ea36
gabriel santos <163359098+gabssiin@users.noreply.github.com>
2025-02-25 00:28:08 +0000

    atualizado!

---
+++ ex01/app/page.js
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+
+function Texto1() {
+  return <p>Um outro parágrafo de texto!</p>;
+}
+
+function gerarNumAleat() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+export default function Home() {
+  const [hide, setHide] = useState(false);
+  const [numAleat, setNumAleat] = useState(gerarNumAleat());
+  const aula = 2;
+  return (
+    <div>
+      <h1>Olá Turma!!!!</h1>
+      <p>Essa é a nossa {aula}ª aula de React!</p>
+      <hr />
+      <button onClick={() => setNumAleat(gerarNumAleat())}>
+        Aletatório: {numAleat}
+      </button>
+      <hr />
+      <button onClick={() => setHide(!hide)}>{hide ? "Show" : "Hide"}</button>
+      <hr />
+      {!hide && (
+        <>
+          <Texto1 />
+          <Texto1 />
+        </>
+      )}
+    </div>
+  );
+}
